619806ce3c03dea114e3f62eef85eadb12d98f07
lelouchZhang <zhch8128@gmail.com>
2026-02-02 00:34:01 +0800

    features/refine data table & data provider impl

--- src/pages/subjects/list.tsx
+++ src/pages/subjects/list.tsx
@@ -1,5 +1,169 @@
+import { CreateButton } from "@/components/refine-ui/buttons/create";
+import { ShowButton } from "@/components/refine-ui/buttons/show";
+import { DataTable } from "@/components/refine-ui/data-table/data-table";
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { ListView } from "@/components/refine-ui/views/list-view";
+import { Badge } from "@/components/ui/badge";
+import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { DEPARTMENTS } from "@/constant";
+import { Subject } from "@/types";
+import { useTable } from "@refinedev/react-table";
+import { ColumnDef } from "@tanstack/react-table";
+import { Search } from "lucide-react";
+import { useMemo, useState } from "react";
+
 const SubjectList = () => {
-  return <div>List</div>;
+  // 使用Refine - Datatable示例
+  // 1.可交互域:下拉菜单 By"department",搜索框 By "name"
+  const [searchQuery, setSearchQuery] = useState("");
+  const [selectedDepartment, setSelectedDepartment] = useState("all");
+
+  // 通过设置FilterFn,实现按需展示数据.
+  const departmentFilters =
+    selectedDepartment === "all"
+      ? []
+      : [
+          {
+            field: "department",
+            operator: "eq" as const,
+            value: selectedDepartment,
+          },
+        ];
+  const searchFilters = searchQuery
+    ? [
+        {
+          field: "name",
+          operator: "contains" as const,
+          value: searchQuery,
+        },
+      ]
+    : [];
+
+  // 建表:DataTable of Subjects
+  const subjectTable = useTable<Subject>({
+    columns: useMemo<ColumnDef<Subject>[]>(
+      () => [
+        {
+          id: "code",
+          accessorKey: "code",
+          size: 100,
+          header: () => <p className="column-title ml-2">Code</p>,
+          cell: ({ getValue }) => <Badge>{getValue<string>()}</Badge>,
+        },
+        {
+          id: "name",
+          accessorKey: "name",
+          size: 200,
+          header: () => <p className="column-title">Name</p>,
+          cell: ({ getValue }) => (
+            <span className="text-foreground">{getValue<string>()}</span>
+          ),
+          filterFn: "includesString",
+        },
+        {
+          id: "department",
+          accessorKey: "department",
+          size: 150,
+          header: () => <p className="column-title">Department</p>,
+          cell: ({ getValue }) => (
+            <Badge variant="secondary">{getValue<string>()}</Badge>
+          ),
+        },
+        {
+          id: "deescription",
+          accessorKey: "description",
+          size: 300,
+          header: () => <p className="column-title">Description</p>,
+          cell: ({ getValue }) => (
+            <span className="truncate">{getValue<string>()}</span>
+            // <span className="line-clamp-2">{getValue<string>()}</span>
+          ),
+        },
+        {
+          id: "details",
+          size: 140,
+          header: () => <p className="column-title">Details</p>,
+          cell: ({ row }) => (
+            <ShowButton
+              resource="subjects"
+              recordItemId={row.original.id}
+              variant="outline"
+              size="sm"
+            >
+              View
+            </ShowButton>
+          ),
+        },
+      ],
+      []
+    ),
+    refineCoreProps: {
+      resource: "subjects",
+      pagination: { pageSize: 10, mode: "server" },
+      filters: {
+        permanent: [...departmentFilters, ...searchFilters],
+      },
+      sorters: {
+        initial: [
+          {
+            field: "id",
+            order: "desc",
+          },
+        ],
+      },
+    },
+  });
+  return (
+    <ListView>
+      <Breadcrumb />
+      <h1 className="page-title">Subject List</h1>
+
+      <div className="intro-row">
+        <p>Quick access to essential metrics and management tools.</p>
+        <div className="actions-row">
+          <div className="search-field">
+            <Search className="search-icon" />
+            <Input
+              type="text"
+              placeholder="Search by name..."
+              className="pl-10 w-full"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+            />
+          </div>
+
+          <div className="flex gap-2 w-full sm:w-auto">
+            <Select
+              value={selectedDepartment}
+              onValueChange={setSelectedDepartment}
+            >
+              <SelectTrigger>
+                <SelectValue placeholder="Filter by department" />
+              </SelectTrigger>
+
+              <SelectContent>
+                <SelectItem value="all">All Departments</SelectItem>
+                {DEPARTMENTS.map((department) => (
+                  <SelectItem key={department} value={department}>
+                    {department}
+                  </SelectItem>
+                ))}
+              </SelectContent>
+            </Select>
+            <CreateButton resource="subjects" />
+          </div>
+        </div>
+      </div>
+      <DataTable table={subjectTable} />
+    </ListView>
+  );
 };
 
 export default SubjectList;
--- src/providers/data.ts
+++ src/providers/data.ts
@@ -1,5 +1,37 @@
-import { createSimpleRestDataProvider } from "@refinedev/rest/simple-rest";
-import { API_URL } from "./constants";
-export const { dataProvider, kyInstance } = createSimpleRestDataProvider({
-  apiURL: API_URL,
-});
+import {
+  BaseRecord,
+  DataProvider,
+  GetListParams,
+  GetListResponse,
+} from "@refinedev/core";
+import { mockSubjectsData } from "./constants";
+
+export const dataProvider: DataProvider = {
+  getList: async <TData extends BaseRecord = BaseRecord>({
+    resource,
+  }: GetListParams): Promise<GetListResponse<TData>> => {
+    if (resource !== "subjects") {
+      return {
+        data: [] as TData[],
+        total: 0,
+      };
+    }
+    return {
+      data: mockSubjectsData as unknown as TData[],
+      total: mockSubjectsData.length,
+    };
+  },
+  getOne: async () => {
+    throw new Error("Method not implemented.");
+  },
+  create: async () => {
+    throw new Error("Method not implemented.");
+  },
+  update: async () => {
+    throw new Error("Method not implemented.");
+  },
+  deleteOne: async () => {
+    throw new Error("Method not implemented.");
+  },
+  getApiUrl: () => "",
+};
